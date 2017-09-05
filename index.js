eb985465ba4c472db6f2063f79dff8fcd8ae4f5f
pat laplante <pat@covenofchaos.com>
2017-09-04 20:18:59 -0400

    an image is added to the pdf

--- index.js
+++ index.js
@@ -1,17 +1,6 @@
-var gm = require('gm').subClass({ imageMagick: true });
-require('gm-base64');
-
-exports.handler = (event, context, callback) => {
-  gm(200, 30, '#fff')
-    .drawText(10, 20, "Hello, World!")
-    .border(2, 2).borderColor('#ff0000')
-    .toBase64('png', function(err, base64) {
-      callback(null, base64);
-    });
-}
-
 const PDFDocument = require('pdfkit');
 const base64 = require('base64-stream');
+var request = require('request');
 
 exports.handler = (event, context, callback) => {
   var doc = new PDFDocument();
@@ -20,17 +9,25 @@ exports.handler = (event, context, callback) => {
   doc.y = 300
   doc.text(event.text, 50, 50)
 
-  var finalString = ''; // contains the base64 string
-  var stream = doc.pipe(base64.encode());
+  request({
+    url: 'https://farm5.staticflickr.com/4347/36617211440_42d47f237a.jpg',
+    encoding: null // Prevents Request from converting response to string
+  }, function(err, response, body) {
 
-  doc.end(); // will trigger the stream to end
-
-  stream.on('data', function(chunk) {
-      finalString += chunk;
-  });
-
-  stream.on('end', function() {
-    // the stream is at its end, so push the resulting base64 string to the response
-    callback(null, finalString);
+    doc.image(body,260, 50,{height:100,width:100});
+    
+    var finalString = ''; // contains the base64 string
+    var stream = doc.pipe(base64.encode());
+  
+    doc.end(); // will trigger the stream to end
+    
+    stream.on('data', function(chunk) {
+        finalString += chunk;
+    });
+  
+    stream.on('end', function() {
+      // the stream is at its end, so push the resulting base64 string to the response
+      callback(null, finalString);
+    });
   });
 }
